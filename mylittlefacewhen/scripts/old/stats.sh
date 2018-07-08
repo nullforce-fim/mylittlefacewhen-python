4c9016dd9478bd0830fd901a600490762f30334f
Glenn <glenngit@nullforce.com>
2018-07-08 18:18:10 -0500

    Nuke it

--- mylittlefacewhen/scripts/old/stats.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-python2.7 /home/inopia/webapps/mylittlefacewhen/mylittlefacewhen/manage.py importlogs
-python2.7 /home/inopia/webapps/mylittlefacewhen/mylittlefacewhen/manage.py updateviews
-#python2.7 /home/inopia/webapps/mylittlefacewhen/mylittlefacewhen/manage.py retirefaces
